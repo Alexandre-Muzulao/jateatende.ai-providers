01c2d93e1a7260820fdb9876078c96abfae6fdc0
Daniel de Sá <daniel.sa@capitanigroup.com>
2025-05-26 20:40:50 -0400

    Refatoração de fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,19 +1,37 @@
 import localFont from 'next/font/local';
 
-export const ampleSoft = localFont(
-  {
-    variable: '--font-ample-soft',
-    src: [
-      {
-        path: '../../public/fonts/AmpleSoftProRegular.ttf',
-        weight: '400',
-        style: 'normal',
-      },
-      {
-        path: '../../public/fonts/AmpleSoftProMedium.ttf',
-        weight: '700',
-        style: 'normal',
-      },
-    ],
-  }
-);
+export const ampleSoft = localFont({
+  variable: '--font-ample-soft',
+  src: [
+    {
+      path: '../../public/fonts/AmpleSoftPro-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/AmpleSoftPro-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/AmpleSoftPro-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/AmpleSoftPro-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/AmpleSoftPro-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/AmpleSoftPro-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
