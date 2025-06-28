cc349f6e62eea4a45bd2ecb2cd1d682f397e15a4
Daniel de Sá <daniel.sa@capitanigroup.com>
2025-06-27 20:14:56 -0400

    estrutura base de assinatura

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   UserGroupIcon,
@@ -6,21 +6,47 @@ import {
   DocumentDuplicateIcon,
   WrenchScrewdriverIcon,
   WrenchIcon,
-  AdjustmentsHorizontalIcon
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+  AdjustmentsHorizontalIcon,
+  BanknotesIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon, Tooltip: 'Home' },
-  { name: 'Meu Perfil', href: '/dashboard/perfil', icon: AdjustmentsHorizontalIcon, Tooltip: 'Seus dados' },
-  { name: 'Serviços', href: '/dashboard/portifolios', icon: WrenchScrewdriverIcon, Tooltip: 'Todos os serviços' },
-  { name: 'Atendimentos', href: '/dashboard/services', icon: WrenchIcon, Tooltip: 'Registro dos atendimentos' },
-  { name: 'Clientes', href: '/dashboard/customers', icon: UserGroupIcon, Tooltip: 'Clientes' },
-  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon, Tooltip: 'Invoices' },
+  { name: "Home", href: "/dashboard", icon: HomeIcon, Tooltip: "Home" },
+  {
+    name: "Meu Perfil",
+    href: "/dashboard/perfil",
+    icon: AdjustmentsHorizontalIcon,
+    Tooltip: "Seus dados",
+  },
+  {
+    name: "Serviços",
+    href: "/dashboard/portifolios",
+    icon: WrenchScrewdriverIcon,
+    Tooltip: "Todos os serviços",
+  },
+  {
+    name: "Atendimentos",
+    href: "/dashboard/services",
+    icon: WrenchIcon,
+    Tooltip: "Registro dos atendimentos",
+  },
+  {
+    name: "Clientes",
+    href: "/dashboard/customers",
+    icon: UserGroupIcon,
+    Tooltip: "Clientes",
+  },
+  {
+    name: "Assinatura",
+    href: "/dashboard/signature",
+    icon: BanknotesIcon,
+    Tooltip: "Assinatura",
+  },
 ];
 
 export default function NavLinks() {
@@ -35,10 +61,10 @@ export default function NavLinks() {
             href={link.href}
             title={link.Tooltip}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-warning hover:text-black md:flex-none md:justify-start md:p-2 md:px-3',
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-warning hover:text-black md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                'bg-warning text-black': pathname === link.href,
-              },
+                "bg-warning text-black": pathname === link.href,
+              }
             )}
           >
             <LinkIcon className="w-6" />
