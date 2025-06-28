9c721339588df858fa4419496cd4bdab3d2d0ea8
Daniel de Sá <daniel.sa@capitanigroup.com>
2025-06-28 16:39:07 -0400

    refatoração de refactor

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,52 @@
-import { NextRequest } from "next/server";
+import { NextURL } from "next/dist/server/web/next-url";
+import { NextRequest, NextResponse } from "next/server";
+import { jwtDecrypt, JWTPayload } from "jose";
+import { auth } from "./auth";
 
-export function middleware(request: NextRequest) {
-  const isLoggedIn = !!auth?.user;
-  if (isLoggedIn) {
-    const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-    if (isOnDashboard) return true;
-    return NextResponse.redirect(new URL("/dashboard", nextUrl));
+const { JWT_SECRET } = process.env;
+const publicPaths = ["/", "/register"];
+
+interface AccessTokenData extends JWTPayload {
+  name: string;
+  email: string;
+  role: "CUSTOMER" | "PROVIDER" | "ADMIN";
+  account: {
+    updatedAt: Date;
+    status: "ACTIVE" | "INACTIVE" | "POST_REGISTRATION";
+  };
+}
+
+async function getAccessTokenData(): Promise<AccessTokenData | null> {
+  const accessToken = (await auth())?.accessToken;
+  if (accessToken) {
+    const secretKey = new TextEncoder().encode(JWT_SECRET);
+    try {
+      const { payload }: { payload: AccessTokenData } = await jwtDecrypt(
+        accessToken,
+        secretKey
+      );
+
+      return payload;
+    } catch (error) {
+      return null;
+    }
   }
-  return false;
+  return null;
+}
+
+export async function middleware({ nextUrl }: { nextUrl: NextURL }) {
+  const accessTokenData = await getAccessTokenData();
+  const isLoggedIn = !!accessTokenData;
+
+  if (isLoggedIn) {
+    if (publicPaths.includes(nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/dashboard", nextUrl));
+    }
+  } else if (!publicPaths.includes(nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/", nextUrl));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
